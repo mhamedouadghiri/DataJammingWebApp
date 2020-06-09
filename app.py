9b19f6a40bfc6e81ca761d3897f273c3a369c2f0
SEFIANE-EL OUADGHIRI M'hamed <56483502+mhamedouadghiri@users.noreply.github.com>
2020-06-09 01:18:07 +0100

    added accuracy - text format

--- app.py
+++ app.py
@@ -15,17 +15,29 @@ def _get_data():
     message = request.form['message']
     result = compute_cipher(message)
     prediction = compute_prediction(result)
+    accuracy = compute_accuracy(message, prediction)
     return render_template('index.html',
-    	message=message, result=result, prediction=prediction)
+    	message=message, result=result, prediction=prediction, accuracy=accuracy)
 
 
 def compute_cipher(message):
 	result = cipher(message)
 	return result
 
+
 def compute_prediction(toDecode):
 	res = decode_sequence(process_text(toDecode), len(toDecode))
 	return res
+
+
+def compute_accuracy(message, predition):
+	same = 0
+	for u, v in zip(message, predition):
+		if u == v:
+			same += 1
+	ratio = same / len(message)
+	percentage = ratio * 100
+	return round(percentage, 2)
 
 
 if __name__ == '__main__':
